6652745823a2c283af6bf689a90f87c0a0031db1
harshitbadolla <harshitbadolla@yahoo.com>
2022-02-19 12:38:18 +0530

    added margin

--- src/components/cartComponents/EmptyCart.js
+++ src/components/cartComponents/EmptyCart.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 export const EmptyCart = () => {
     return (
         <div className="empty-cart">
-            <div className="flex-col gap flex-all-center">
+            <div className="flex-col my-1 flex-all-center">
                 <h1>Hey, it feels so light!</h1>
                 <p>There is nothing in your bag. Let's add some items.</p>
             </div>
-            <div className="flex-row gap flex-all-center">
+            <div className="flex-row my-1 flex-all-center">
                 <Link to="/products">
                     <h3 className="btn btn-primary text-uppercase">Go Shopping</h3>
                 </Link> 
