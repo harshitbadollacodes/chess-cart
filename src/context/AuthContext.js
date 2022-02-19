dcf9fc5ddac6e2d2501d7934e4c51d4e9a37402b
harshitbadolla <harshitbadolla@yahoo.com>
2022-02-19 12:56:32 +0530

    added logout upon token expiry

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -19,6 +19,24 @@ export function AuthProvider({ children }){
     const [token, setToken] = useState(savedToken);
     const [userId, setUserId] = useState(savedUserId);
 
+    axios.interceptors.response.use((response) => response,
+        (error) => {
+            if (error?.response?.status === 401) {
+                logout();
+            }
+
+            return Promise.reject(error);
+        }
+    );
+
+    function logout() {
+        setIsUserLoggedIn(false);
+        setToken(null);
+        setUserId(null);
+        localStorage.removeItem("userId");
+        localStorage.removeItem("token");
+    }
+
     return (
         <AuthContext.Provider 
             value={{ 
