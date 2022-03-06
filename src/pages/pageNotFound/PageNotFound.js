6d97abea0fbf6e9b0b0b581736696ab655fda69c
harshitbadolla <harshitbadolla@yahoo.com>
2022-03-06 16:22:42 +0530

    Update:  Design change for 404 page

--- src/pages/pageNotFound/PageNotFound.js
+++ src/pages/pageNotFound/PageNotFound.js
@@ -1,7 +1,18 @@
+import "./pageNotFound.css";
+import { Link } from "react-router-dom";
+
 export const PageNotFound = () => {
     return (
-        <div className="container">
-            <h1>404....Page Not Found</h1>
+        <div className="container flex-col items-center">
+            <h1 className="text-large text-300">404</h1>
+            <h3 className="text-l text-300 my-1 text-center">Page Not Found</h3>
+            <p className="text-center">We're sorry the page you requested could not be found.Please go back to the homepage</p>
+            <Link 
+                to="/"
+                className="btn btn-primary my-1 cursor-pointer"
+            >
+                Home
+            </Link>
         </div>
     );
 }
