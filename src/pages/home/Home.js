fd76afe816cba44b48fe8987af468d9fa3872a83
harshitbadolla <harshitbadolla@yahoo.com>
2022-03-20 22:06:04 +0530

    Update: Home page design

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,26 @@
 import "./home.css";
+import chessKnight from "../../images/chessKnight.png"
 import { Link } from "react-router-dom";
 
 export function Home() {
     return (
-        <div className="banner">
-            <div className="bg-overlay">
-                <div className="flex-col flex-all-center height-100">
-                    <Link to="/products" className="btn btn-primary my-1 w-20">Products</Link>
-                    <Link to="/login" className="btn btn-primary my-1 w-20">Login</Link>
+        <>
+            <header className="banner">
+                <div className="bg-overlay">
+                    <div className="flex-col flex-all-center height-100">
+                        <h1 className="l-heading text-white">
+                            Welcome to Chess Cart 
+                            <span>
+                                <img src={chessKnight} className="header-logo" alt="banner"/>
+                            </span> 
+                        </h1>
+                        <p className="tagline text-white">A one stop shop for Chess lovers.</p>
+                        <div className="flex flex-row">
+                            <Link to="/products" className="btn bg-white my-1 w-20">Explore</Link>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </header>
+        </>
     )
 }
