427e707ce482417675953bd2b2069a95d903324a
harshitbadolla <harshitbadolla@yahoo.com>
2022-04-03 19:40:14 +0530

    beautified the code

--- src/components/productComponents/Mobile/SortFilterButtons.js
+++ src/components/productComponents/Mobile/SortFilterButtons.js
@@ -13,8 +13,18 @@ export const SortFilterButtons = () => {
     return (
         <div className="m3 hide-on-mobile">
             <div className="flex-row flex-all-center p1 sort-filter-btn">
-                <button  onClick={() => setDisplay({...display, sort: !display.sort, filter: false})} className="text-s text-uppercase sortBtn">sort</button>
-                <button  onClick={() => setDisplay({...display, filter: !display.filter, sort: false})} className="text-s text-uppercase filterBtn">Filter</button>
+                <button 
+                    onClick={() => setDisplay({...display, sort: !display.sort, filter: false})} 
+                    className="text-s text-uppercase sortBtn"
+                >
+                    Sort
+                </button>
+                <button 
+                    onClick={() => setDisplay({...display, filter: !display.filter, sort: false})} 
+                    className="text-s text-uppercase filterBtn"
+                >
+                    Filter
+                </button>
             </div>
 
             { display.sort === true && <Sort/> }
